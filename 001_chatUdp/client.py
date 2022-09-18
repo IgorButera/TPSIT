52a504b710dfc46aefc775fe882135669512a0d9
IgorButera <94286631+IgorButera@users.noreply.github.com>
2022-09-18 20:00:13 +0200

    Update client.py

--- 001_chatUdp/client.py
+++ 001_chatUdp/client.py
@@ -2,10 +2,10 @@ from socket import AF_INET, SOCK_DGRAM, socket
 
 def chatClient():
     with socket(AF_INET, SOCK_DGRAM) as s:
-        msg = "zitto zakka"
-        msg = msg.encode('UTF8')
-
-        s.sendto(msg, ("192.168.95.255", 5000))
+        while True:
+            msg = input("Scrivi un messaggio: ")
+            msg = msg.encode('UTF8')
+            s.sendto(msg, ("192.168.95.255", 5000))
 
 if __name__ == "__main__":
     chatClient()
